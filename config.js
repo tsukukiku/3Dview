7122ec361b30d8d9f57db06f0c0ada0ffc56b3d6
tsukukiku <zhanghaiyanxgs@yahoo.co.jp>
2026-05-16 16:10:11 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.APP_CONFIG = {
-  // Same origin by default. For GitHub Pages, set this to your Synology HTTPS API.
-  // Example: "http://tsukukiku.synology.me:8081"
-  API_BASE_URL: ""
+  API_BASE_URL: "http://tsukukiku.synology.me:8081"
 };
